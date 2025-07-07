e81b30f3e5f4a2fbf0a65813109a8a6c320c2648
ItsEhsanMM <itsehsanmm@gmail.com>
2025-07-07 16:42:36 +0330

    add special

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import MobileNav from "./components/MobileNav";
+import Special from "./components/Special";
 
 function App() {
    return (
       <div
-         className="font-yekan custom-container bg-background"
+         className="font-yekan custom-container bg-background flex flex-col space-y-20"
          dir="rtl"
       >
          <Header />
          <MobileNav />
          <Hero />
+         <Special />
       </div>
    );
 }
